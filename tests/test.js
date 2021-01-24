555801051ed59fb0be5ce15c7065f6eeb1b3ea81
ejmorgan <admin@ejmorgan.com>
2021-01-23 19:11:07 -0800

    Fix test

--- tests/test.js
+++ tests/test.js
@@ -6,19 +6,18 @@ describe("button#cta", function() {
   describe("click", function() {
     it("should change p's innerText to 'You clicked it!'", async function() {
       const browser = await puppeteer.launch({
-        args: ["--no-sandbox"]
+        args: ["--no-sandbox"],
       });
       const page = await browser.newPage();
       await page.goto(`file://${path.join(path.resolve("."), "circleci.html")}`);
 
       const btn = await page.$("#cta");
-      btn.click();
+      await btn.click();
 
-      page.$eval("#text", function(el) {
-        expect(el.innerText).to.equal("You clicked it!");
-      })
-
+      const innerText = await page.$eval("#text", el => el.innerText)
       await browser.close();
+
+      expect(innerText).to.equal("You clicked it!");
     })
   })
 })
